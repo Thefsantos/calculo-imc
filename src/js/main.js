7c347f17d3006d44ced2ac8c098df74cd106ea19
Thefsantos <francisconomade491@gmail.com>
2023-06-07 10:41:38 -0300

    Finishing

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,34 @@
-// VARIÁVEIS
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { IMC, notNumber} from './utils.js'
+
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-// callback
-form.onsubmit = (e) => {
+ 
+form.onsubmit = e => {
     e.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
+
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+
+    if(weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return
+    }
+
+    AlertError.close()
+
+    const result = IMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+
+    Modal.open()
+    Modal.message.innerText = message
 }
